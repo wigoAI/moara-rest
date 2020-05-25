11bc2eb221a67fa9db5890ecb832c61c651ca48f
macle <ysys86a@gmail.com>
2020-05-25 18:54:24 +0900

    vacab 이벤트 업데이트

--- src/main/java/org/moara/rest/api/ml/VocabController.java
+++ src/main/java/org/moara/rest/api/ml/VocabController.java
@@ -31,11 +31,11 @@ public class VocabController {
 
     private static final Logger logger = LoggerFactory.getLogger(VocabController.class);
 
-    @RequestMapping(value = "/ml/vocab/tokenize" , method = RequestMethod.POST, produces= MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/ml/vocab/tokenize" , method = RequestMethod.POST, produces= MediaType.ALL_VALUE)
     public String documentWordSimple(@RequestBody String jsonValue){
 
 
-        if("".equals(jsonValue)){
+        if("".equals(jsonValue.trim())){
             return "[]";
         }
         try {
