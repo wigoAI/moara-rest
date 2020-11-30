1bb6c089be7996b4615fbf1b0548865e1bbb75d0
macle <ysys86a@gmail.com>
2020-11-30 16:11:30 +0900

    rest api 추가 (감성 분류 nipa 연동용) 의존성 업데이트

--- src/main/java/org/moara/rest/api/nlp/ClassifyApiController.java
+++ src/main/java/org/moara/rest/api/nlp/ClassifyApiController.java
@@ -16,7 +16,17 @@
 
 package org.moara.rest.api.nlp;
 
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
+import org.moara.ara.classification.model.ClassificationModel;
+import org.moara.ara.classification.model.ClassificationModelMetaManager;
+import org.moara.ara.classification.model.SimpleClassificationResults;
+import org.moara.ara.datamining.data.CodeName;
+import org.moara.ara.datamining.textmining.TextMining;
 import org.moara.ara.datamining.textmining.api.classify.ClassifyDefaultApi;
+import org.moara.ara.datamining.textmining.api.document.JsonDocument;
+import org.moara.ara.datamining.textmining.document.Document;
+import org.moara.common.config.Config;
 import org.moara.common.util.ExceptionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +35,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * 분류 api 정의
@@ -52,5 +65,72 @@ public class ClassifyApiController {
 			return "";
 		}
 	}
-	
+
+	@RequestMapping(value = "/classify/emotion" , method = RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	public String emotion(@RequestBody String documentJson) {
+		try {
+
+			JSONObject result = new JSONObject();
+
+			Document document = JsonDocument.makeDocument(documentJson);
+			TextMining.mining(document);
+
+			String rootCode = Config.getConfig("emotion.positive.code", "U716");
+			Collection<ClassificationModel<?>> classificationModels = ClassificationModelMetaManager.getInstance().getClassificationModels();
+			for (ClassificationModel<?> model : classificationModels) {
+				if (!model.autoClassification()) continue;
+
+				if(!model.getRootCategoryCode().equals(rootCode)){
+					continue;
+				}
+
+				SimpleClassificationResults results = new SimpleClassificationResults(model);
+				Map<String, JsonObject> predictResult = results.predictResult(document);
+				JsonObject jsonObject = predictResult.get(Config.getConfig("emotion.positive.code", "U716001"));
+				if (jsonObject == null) {
+					result.put("positive", 0.0);
+				}else{
+					if(jsonObject.has("probability")){
+						result.put("positive", jsonObject.get("probability").getAsDouble());
+					}else{
+						result.put("positive", 0.0);
+					}
+
+				}
+
+				jsonObject = predictResult.get(Config.getConfig("emotion.negative.code", "U716002"));
+				if (jsonObject == null) {
+					result.put("negative", 0.0);
+				}else{
+					if(jsonObject.has("probability")){
+						result.put("negative", jsonObject.get("probability").getAsDouble());
+					}else{
+						result.put("negative", 0.0);
+					}
+
+				}
+
+				jsonObject = predictResult.get(Config.getConfig("emotion.neutral.code", "U716002"));
+				if (jsonObject == null) {
+					result.put("neutral", 0.0);
+				}else{
+					if(jsonObject.has("probability")){
+						result.put("neutral", jsonObject.get("probability").getAsDouble());
+					}else{
+						result.put("neutral", 0.0);
+					}
+
+				}
+				break;
+			}
+
+			return result.toString();
+		}catch(Exception e) {
+			logger.error(ExceptionUtil.getStackTrace(e));
+			return "{}";
+		}
+	}
+
+
+
 }
